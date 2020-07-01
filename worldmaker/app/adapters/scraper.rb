2f30862ad4baff4e1478adefda52acbb7e0fca4d
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-01 15:23:28 -0400

    scraper docs

--- worldmaker/app/adapters/scraper.rb
+++ worldmaker/app/adapters/scraper.rb
@@ -4,31 +4,14 @@ require 'pry'
 
 class Scraper
 
-    def self.fetch(name)
-        website = "www.dnd5eapi.co/api/spells/#{name}"
-        begin
-        response = RestClient.get(website)
-      rescue
-        puts "-----------------------------------------".colorize(:red)
-        puts "Sorry, that monster doesn't exist!".colorize(:red)
-        puts "Please enter a new monster.".colorize(:red)
-        puts "-----------------------------------------".colorize(:red)
-        input=gets.chomp.downcase
-        self.fetch(input)
-      else
-        response = JSON.parse(response)
+      url = https://open5e.com/magicitems/magicitem-list/search/
 
-          hash  = {
-          "name" => response["name"],
-          "desc" => response["desc"],
-          "page" => response["page"],
-          "material" => response["material"],
-          "classes" => response["classes"],
-          "url" => response["url"]
-        }
+      html = open(url)
+      
+      doc = Nokogiri::HTML(html)
 
-          DND::Monster.new(hash)
-        end
-    end
+      binding.pry
 
 end
+
+scrape = Scraper.new
