4cbc8add5659765f318866937dc7fa95d41c7de5
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-03 14:05:51 -0400

    changing sessions controller to log in thru twit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,9 @@ class SessionsController < ApplicationController
     end
   
     def create # receives data submitted in login form, authenticates and logs in a valid user
-      if auth_hash = request.env["omniauth.auth"]
-        @user = User.find_or_create_by_omniauth(auth_hash)
+      if @user = User.find_or_create_from_auth_hash(auth_hash)
         session[:user_id] = @user.id
-        redirect_to user_path(@user), flash: { success: "You successfully logged in!" }
+        redirect_to root_path  
       else
         @user = User.find_by(email: params[:email])
         if @user && @user.authenticate(params[:password])
@@ -32,10 +31,14 @@ class SessionsController < ApplicationController
     ####################### END ROUTES #####################
   
     private
+
+    protected
+
+    def auth_hash
+      request.env['omniauth.auth']
+    end
   
     def prevent_logged_in_users_from_viewing_login
       redirect_to root_path, alert: "You cannot view the login form since you're already logged in!" if current_user
     end
 end
-  
-  end
